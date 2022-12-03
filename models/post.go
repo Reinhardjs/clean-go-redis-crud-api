baa9f036619425abdcd9836c55a1be5f800a5b62
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-03 15:27:03 +0700

    chore(dot-crud-redis-go-api): refactor get posts model return

--- models/post.go
+++ models/post.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"dot-crud-redis-go-api/configs"
+	"fmt"
 
 	"github.com/jinzhu/gorm"
 )
@@ -19,7 +20,7 @@ func GetPosts() ([]*Post, error) {
 	posts := make([]*Post, 0)
 	err := DB.Table("posts").Find(&posts).Error
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("DB error : %v", err)
 	}
 
 	return posts, nil
