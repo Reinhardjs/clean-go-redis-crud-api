83dcdad4f70327647ab96118548250cf4a1e98f7
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-03 14:20:29 +0700

    chore(dot-crud-redis-go-api): added GetPosts in Post model

--- models/post.go
+++ models/post.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"dot-crud-redis-go-api/configs"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -8,4 +10,17 @@ type Post struct {
 	gorm.Model
 	Title       string `json:"title"`
 	Description string `json:"description"`
+}
+
+func GetPosts() (error, []*Post) {
+
+	DB := configs.GetDB()
+
+	posts := make([]*Post, 0)
+	err := DB.Table("posts").Find(&posts).Error
+	if err != nil {
+		return err, nil
+	}
+
+	return nil, posts
 }
