e2525ebfe660c9207d40984fc5645455610986f5
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-03 16:58:53 +0700

    chore(dot-crud-redis-go-api): added GetPost in post's controller

--- controllers/controller_post.go
+++ controllers/controller_post.go
@@ -6,7 +6,10 @@ import (
 	"dot-crud-redis-go-api/responses"
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func GetPosts() http.Handler {
@@ -23,6 +26,33 @@ func GetPosts() http.Handler {
 		}
 
 		response := responses.FineResponse{Status: http.StatusOK, Message: "success", Data: posts}
+		rw.WriteHeader(response.Status)
+		json.NewEncoder(rw).Encode(response)
+		return nil
+	})
+}
+
+func GetPost() http.Handler {
+	return RootHandler(func(rw http.ResponseWriter, r *http.Request) (err error) {
+		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		params := mux.Vars(r)
+		id, err := strconv.Atoi(params["id"])
+
+		if err != nil {
+			return err
+		}
+
+		rw.Header().Add("Content-Type", "application/json")
+
+		post, err := models.GetPost(uint(id))
+
+		if err != nil {
+			return err
+		}
+
+		response := responses.FineResponse{Status: http.StatusOK, Message: "success", Data: post}
 		rw.WriteHeader(response.Status)
 		json.NewEncoder(rw).Encode(response)
 		return nil
