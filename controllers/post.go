71b8897ab7abaa52072663ce9fcae7752a1fa4fd
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-03 21:47:48 +0700

    chore(dot-crud-redis-go-api): added update post controller method

--- controllers/post.go
+++ controllers/post.go
@@ -96,3 +96,43 @@ func CreatePost() http.Handler {
 		return nil
 	})
 }
+
+func UpdatePost() http.Handler {
+	return RootHandler(func(rw http.ResponseWriter, r *http.Request) (err error) {
+		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		rw.Header().Add("Content-Type", "application/json")
+
+		params := mux.Vars(r)
+		postId, err := strconv.Atoi(params["postId"])
+
+		if err != nil {
+			return utils.NewHTTPError(nil, 400, "Invalid post id")
+		}
+
+		post := &models.Post{}
+		post.ID = postId
+		decodeError := json.NewDecoder(r.Body).Decode(post)
+		if decodeError != nil {
+			return utils.NewHTTPError(nil, 400, "Invalid request body format")
+		}
+
+		if r.Method == "PUT" {
+			if message, ok := post.Validate(); !ok {
+				return utils.NewHTTPError(nil, 400, message)
+			}
+		}
+
+		result, err := post.Update()
+
+		if err != nil {
+			return err
+		}
+
+		response := responses.FineResponse{Status: http.StatusOK, Message: "success", Data: result}
+		rw.WriteHeader(response.Status)
+		json.NewEncoder(rw).Encode(response)
+		return nil
+	})
+}
