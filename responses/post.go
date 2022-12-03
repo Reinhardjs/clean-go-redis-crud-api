cc2dacea1a127efb850cb2a30c02525b31b72dc7
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-03 21:46:38 +0700

    chore(dot-crud-redis-go-api): added CreatePostResponse and UpdatePostResponse

---
+++ responses/post.go
@@ -0,0 +1,20 @@
+package responses
+
+import (
+	"time"
+)
+
+type CreatePostResponse struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type UpdatePostResponse struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
